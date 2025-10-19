fe67e11044c41bbd80b432c506f3feb10295daf5
Никита Лисичкин <exort1703@list.ru>
2025-10-19 22:23:29 +0300

    final_2

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const container = document.getElementById("main__block")
-const btnReload = document.querySelector(".btn")
+const main__block = document.getElementById("main__block")
+const btnResize = document.querySelector(".btn-resize")
+const btnReload = document.querySelector(".btn-reload")
+
+
 
 function createElement(num){
-
+    // Очищаем существующую сетку
+    main__block.innerHTML = ''
     for (let x = 0; x < num; x++){
         const line = document.createElement("div")
         line.classList.add("line")
@@ -12,7 +16,7 @@ function createElement(num){
             column.classList.add("column")
             line.appendChild(column)
         }
-        container.appendChild(line)
+        main__block.appendChild(line)
         
         line.addEventListener('mouseover', e => {
             if (e.target !== line){
@@ -29,18 +33,39 @@ function createElement(num){
     }
 }
 
+
+let gridSize = 12
 // Обработчик для кнопки сброса
 btnReload.addEventListener('click', () => {
     const columns = document.querySelectorAll('.column');
     columns.forEach(column => {
-        column.style.backgroundColor = '';
+        column.style.backgroundColor =`rgb(255, 255, 255)`;
     });
 });
 
-let grid = prompt(`Введите размер сетки (до 100):`)
-while (grid > 100){
-    grid = prompt(`Превышен лимит размерности сетки. Повтороно введите размер сетки (до 100):`)
-}
+// Обработчик для изменения размерности сетки
+btnResize.addEventListener('click', () => {
+    let input = prompt(`Введите размер сетки (1 - 40):`)
+    if (input === null || isNaN(parseInt(input))){
+        gridSize = 12
+    }
+    else{
+        let newSize = parseInt(input)
+        while (newSize > 40 || newSize < 1){
+            input = prompt(`Превышен лимит размерности сетки. Повтороно введите размер сетки (1 - 40):`)
+            if (input === null || isNaN(parseInt(input))){
+                gridSize = 12
+                break
+            }
+            newSize = parseInt(input)
+        }
+        gridSize = newSize
+    }
+
+    createElement(gridSize)
+});
 
 
-createElement(grid)
+
+
+createElement(gridSize)
